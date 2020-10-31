89ac090f32a32a4d7f4a9cf266fba4fdd33147c2
riqui.tellez <riqui.tellez@optimissa.com>
2020-10-31 21:56:06 +0100

    Project draft 1

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book name")]
+        public string BookName { get; set; }
+        public string Chapter { get; set; }
+        public string Verse { get; set; }
+        [Display(Name = "Study notes")]
+        public string StudyNotes { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Creation date")]
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public async Task OnGetAsync()
+        {
+            var scriptures = from m in _context.Scripture
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.BookName.Contains(SearchString) || s.StudyNotes.Contains(SearchString));
+            }
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
