78bf70978b2387e526e911073d2bdcb790e1ee10
riqui.tellez <riqui.tellez@optimissa.com>
2020-10-31 23:06:26 +0100

    Final changes

--- MyScriptureJournal/Models/Scripture.cs
+++ MyScriptureJournal/Models/Scripture.cs
@@ -9,12 +9,16 @@ namespace MyScriptureJournal.Models
     public class Scripture
     {
         public int ID { get; set; }
+        [Required]
         [Display(Name = "Book name")]
         public string BookName { get; set; }
+        [Required]
         public string Chapter { get; set; }
+        [Required]
         public string Verse { get; set; }
         [Display(Name = "Study notes")]
         public string StudyNotes { get; set; }
+        [Required]
         [DataType(DataType.Date)]
         [Display(Name = "Creation date")]
         public DateTime CreationDate { get; set; }
